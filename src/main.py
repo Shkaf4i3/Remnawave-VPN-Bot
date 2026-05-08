24a927b58ce46d3678dea901345ee5793cd2ae05
Shkaf4i3 <fleximes@protonmail.com>
2026-05-08 17:56:41 +0300

    remove default docs path in FastAPI app

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from .routes import webhook_router
 from .utils import lifespan
 
 
-app = FastAPI(docs_url="/", redoc_url=None, lifespan=lifespan)
+app = FastAPI(redoc_url=None, lifespan=lifespan)
 app.include_router(router=webhook_router, prefix="/api/v1")
 
 
